c6968778bc062783721e6d5fd91540909a3989fd
Janith Lahiru Kariyawasam <88109775+janithlahirukariyawasam@users.noreply.github.com>
2023-03-16 02:35:01 +0530

    Create FunResController.java

---
+++ myapp/src/main/java/com/janithlahiru/project/myapp/rest/FunResController.java
@@ -0,0 +1,12 @@
+package com.janithlahiru.project.myapp.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunResController {
+    @GetMapping("/")
+    public String sayHello(){
+        return "Hello World";
+    }
+}
